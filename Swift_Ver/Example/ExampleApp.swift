624553153bb73c58bd72ef00eb972877e0cc69cf
Deve <tambatd@outlook.com>
2021-04-13 19:14:20 -0400

    Swift Version
    Swift version of RN app

---
+++ Swift_Ver/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Deve on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
